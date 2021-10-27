6609a9f299450ab06617531b9c980c452a1f6e04
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-27 19:43:02 +1100

    Fix button to fixtures#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :clubs, :only => [ :index, :show ]
+
   get '/fixtures' => 'fixtures#index'
   get '/fixtures/:round' => 'fixtures#round', as: 'round'
+  get '/fixtures/:round/:id' => 'fixtures#show', as: 'fixture'
+
 end
