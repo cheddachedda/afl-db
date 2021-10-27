cc7988ce8eb7cc948940cc554e2f773cde7b47d5
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-27 23:39:29 +1100

    Add the behemoth that is the stats scrapers

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ Fixture.destroy_all
 def scrape_fixtures
   url = "https://fixturedownload.com/results/afl-2021"
   unparsed_page = HTTParty.get(url)
-  parsed_page = Nokogiri::HTML(unparsed_page)
+  parsed_page = Nokogiri::HTML(unparsed_page.body)
 
   rows = parsed_page.css('tr').drop 1
 
@@ -75,10 +75,12 @@ puts "#{ Fixture.count } games created and associated"
 
 ################################## PLAYERS #####################################
 
+# I. Scrape for basic player data
+
 def scrape_players
   url = "https://afltables.com/afl/stats/2021.html"
   unparsed_page = HTTParty.get(url)
-  parsed_page = Nokogiri::HTML(unparsed_page)
+  parsed_page = Nokogiri::HTML(unparsed_page.body)
 
   tables = parsed_page.css('.sortable')
   tables.each do |t|
@@ -98,13 +100,134 @@ def scrape_players
 end
 
 scrape_players
-puts "#{ Player.count } players created"
+
+# II. Scrape for all game-by-game stats from afltables.com
+
+def scrape_player_stats player
+  puts "Adding stats for #{ player.name }"
+  first_name = player.first_name
+  last_name = player.last_name
+
+  url = "https://afltables.com/afl/stats/players/#{ first_name[0] }/#{ first_name }_#{ last_name.gsub(' ', '_') }.html"
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page.body)
+
+  result = false
+  unless parsed_page.css('.sortable').empty?
+
+    rows = parsed_page.css('.sortable').last.css('tbody')[0].css('tr')
+
+    categories = [ :kicks, :marks, :handballs, :goals, :behinds, :hit_outs, :tackles, :free_kicks_for, :free_kicks_against ]
+
+    # Initialise player hash
+    player = {}
+    categories.each do |cat|
+      player[:name] = "#{ first_name } #{ last_name }"
+      player[cat] = []
+    end
+
+    # Add each stat
+    rows.each do |row|
+      data = row.css('td')
+
+      # Adds stat each a specific `round` in order to create nil values for DNPs
+      round = case data[2].text
+      when 'EF'
+        round = 24
+      when 'SF'
+        round = 25
+      when 'PF'
+        round = 26
+      when 'GF'
+        round = 27
+      else
+        round = data[2].text.to_i
+      end
+
+      player[:kicks][round - 1] = data[5].text.to_i
+      player[:handballs][round - 1] = data[6].text.to_i
+      player[:marks][round - 1] = data[7].text.to_i
+      player[:goals][round - 1] = data[9].text.to_i
+      player[:behinds][round - 1] = data[10].text.to_i
+      player[:hit_outs][round - 1] = data[11].text.to_i
+      player[:tackles][round - 1] = data[12].text.to_i
+      player[:free_kicks_for][round - 1] = data[17].text.to_i
+      player[:free_kicks_against][round - 1] = data[18].text.to_i
+    end
+
+    # Updates each Player model
+    id = Player.find_by(name: "#{last_name}, #{first_name}").id
+    player.keys.drop(1).each do |key|
+      Player.update id, key => player[key]
+    end
+
+    result = true
+  end
+
+  result
+end
+
+stats_scrape_count = 0
+Player.all.each do |player|
+  if scrape_player_stats player
+    stats_scrape_count += 1
+  end
+end
+puts "#{ stats_scrape_count } of #{ Player.count } players' stats scraped"
+
+# III. Scrape for fantasy positions
+
+# Web Scrape
+def fantasy_scraper
+  url = "http://dtlive.com.au/afl/dataview.php"
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page.body)
+
+  rows = parsed_page.css('tr')
+  players = []
+
+  # Iterate through all rows (except 1 header row and 1 footer row)
+  (1..rows.count-2).each do |i|
+    player = {
+      name: rows[i].children[1].text,
+      position: rows[i].children[2].text.upcase.split(','),
+      fantasy_scores: (14..36).map { |col|
+        rows[i].children[col].text.empty? ?
+        nil : rows[i].children[col].text.to_i
+      }
+    }
+    player[:games] = player[:fantasy_scores].select{|n|n}.count
+    player[:average] = player[:fantasy_scores].select{|n|n}.sum.to_f / player[:games]
+
+    players << player
+  end
+
+  count = 0
+  # Updates each Player model
+  players.each do |player|
+    id = nil
+
+    if Player.find_by(expected_dtlive_alias: player[:name])
+      id = Player.find_by(expected_dtlive_alias: player[:name]).id
+    end
+
+    unless id == nil
+      puts "Updating fantasy info for #{ Player.find(id).name }"
+      Player.update id, :position => player[:position], :fantasy_scores => player[:fantasy_scores]
+      count += 1
+    end
+  end
+  puts "#{ count } of #{ Player.count } players' dtlive data added"
+end
+
+fantasy_scraper
 
 ####################### ASSOCIATE FIXTURES + PLAYERS ###########################
 
 puts "Associating Fixtures and Players"
 fixtures_and_players_start_time = Time.new
 Player.all.each do |player|
+  puts "Associating fixtures for #{ player.name }"
   player.club.fixtures.each do |fixture|
     player.fixtures << fixture
   end
