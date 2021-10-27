fc3f47b52f7fb2fd102fb55387e94a3ee06898ed
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-27 13:03:18 +1100

    Remove Club new,create,edit,update,destroy

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -3,23 +3,7 @@ class ClubsController < ApplicationController
     @clubs = Club.all
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def show
     @club = Club.find params[:id]
-    @keys = [ :id, :name, :abbreviation, :fixtures_alias, :afl_tables_alias, :image ]
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
