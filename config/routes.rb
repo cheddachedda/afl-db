50a085f6bfd0e370795e2355c5dcd0e49c3f8b88
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-27 20:57:05 +1100

    Generate Player controller, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :clubs, :only => [ :index, :show ]
+  resources :players
 
   get '/fixtures' => 'fixtures#index'
   get '/fixtures/:round' => 'fixtures#round', as: 'round'
