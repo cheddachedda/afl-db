f31f025e6bcd656462457623436c2ccabe398b47
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-01 06:42:22 +1100

    Refactor Club methods

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,64 +2,16 @@ class Club < ApplicationRecord
   has_and_belongs_to_many :fixtures
   has_many :players
 
-  def home_fixtures
-    fixtures = self.fixtures.where(home_id: self.id)
-  end
-
-  def away_fixtures
-    fixtures = self.fixtures.where(away_id: self.id)
-  end
-
-  def home_wins
-    self.home_fixtures.filter{|f| f.home_win && f.is_regular_season}.count
-  end
-
-  def home_draws
-    self.home_fixtures.filter{|f| f.home_draw && f.is_regular_season}.count
-  end
-
-  def home_losses
-    self.home_fixtures.filter{|f| f.home_loss && f.is_regular_season}.count
-  end
-
-  def home_points_for
-    self.home_fixtures.filter{|f| f.is_regular_season}.pluck(:home_score).sum
-  end
-
-  def home_points_against
-    self.home_fixtures.filter{|f| f.is_regular_season}.pluck(:away_score).sum
-  end
-
-  def away_wins
-    self.away_fixtures.filter{|f| f.away_win && f.is_regular_season}.count
-  end
-
-  def away_draws
-    self.away_fixtures.filter{|f| f.away_draw && f.is_regular_season}.count
-  end
-
-  def away_losses
-    self.away_fixtures.filter{|f| f.away_loss && f.is_regular_season}.count
-  end
-
-  def away_points_for
-    self.away_fixtures.filter{|f| f.is_regular_season}.pluck(:away_score).sum
-  end
-
-  def away_points_against
-    self.away_fixtures.filter{|f| f.is_regular_season}.pluck(:home_score).sum
-  end
-
   def wins
-    self.home_wins + self.away_wins
+    self.fixtures.filter{ |f| f.win? self }.count
   end
 
   def draws
-    self.home_draws + self.away_draws
+    self.fixtures.filter{ |f| f.draw? }.count
   end
 
   def losses
-    self.home_losses + self.away_losses
+    self.fixtures.filter{ |f| f.loss? self }.count
   end
 
   def record
@@ -71,11 +23,15 @@ class Club < ApplicationRecord
   end
 
   def points_for
-    self.home_points_for + self.away_points_for
+    home_points = self.fixtures.filter{ |f| f.home_id == self.id }.pluck(:home_score).sum
+    away_points = self.fixtures.filter{ |f| f.away_id == self.id }.pluck(:away_score).sum
+    home_points + away_points
   end
 
   def points_against
-    self.home_points_against + self.away_points_against
+    home_points = self.fixtures.filter{ |f| f.home_id == self.id }.pluck(:away_score).sum
+    away_points = self.fixtures.filter{ |f| f.away_id == self.id }.pluck(:home_score).sum
+    home_points + away_points
   end
 
   def percentage
