980fd883fd8ef52f3400fef99a58bca6bbf35b9f
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-31 21:05:06 +1100

    Remove fixture-player associations for nils

---
+++ db/seeds/06_associate_fixtures_players.rb
@@ -0,0 +1,21 @@
+require 'pry'
+puts "Associating Fixtures and Players"
+@associated_fixtures = 0
+@associated_players = 0
+
+Player.all.each do |player|
+  player.percentage_time_on_ground.each_with_index do |value, index|
+    unless value.nil?
+      fixture = player.club.fixtures.find_by(round_id: index + 1)
+      unless fixture.nil?
+        player.fixtures << fixture
+        @associated_fixtures += 1
+        puts "Associated #{ fixture.matchup } with #{ player.name }"
+      end
+    end
+  end
+
+  @associated_players += 1
+end
+
+puts "Associated #{ @associated_fixtures } fixtures for #{ @associated_players } players"
