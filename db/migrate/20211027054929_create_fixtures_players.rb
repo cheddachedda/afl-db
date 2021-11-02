262d047d44a7db49883576067271ec28ba3bbc7c
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-03 09:37:06 +1100

    Remove fixtures-players associations to reduce database size

--- db/migrate/20211027054929_create_fixtures_players.rb
+++
@@ -1,8 +0,0 @@
-class CreateFixturesPlayers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :fixtures_players, :id => false do |t|
-      t.integer :fixture_id
-      t.integer :player_id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_054929) do
+ActiveRecord::Schema.define(version: 2021_10_27_054918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 2021_10_27_054929) do
     t.integer "home_score"
     t.integer "away_id"
     t.integer "away_score"
-  end
-
-  create_table "fixtures_players", id: false, force: :cascade do |t|
-    t.integer "fixture_id"
-    t.integer "player_id"
   end
 
   create_table "players", force: :cascade do |t|
