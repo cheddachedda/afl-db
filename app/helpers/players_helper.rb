007ef8589486a746058e37dc7398988d10e8a891
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-04 11:39:29 +1100

    Add helper .get_score_class to apply colours to scores

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,19 @@
 module PlayersHelper
+  def get_score_class score
+    if score.nil?
+      return
+    elsif score >= 150
+      return 'score-elite'
+    elsif score >= 100
+      return 'score-high'
+    elsif score >= 75
+      return 'score-good'
+    elsif score >= 50
+      return 'score-average'
+    elsif score >= 25
+      return 'score-low'
+    else
+      return 'score-poor'
+    end
+  end
 end
