aeb267886a3c131abaf1ece57105c55f54705a58
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-06 18:44:24 +1100

    Adjust score thresholds for each class

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -2,15 +2,15 @@ module PlayersHelper
   def get_score_class score
     if score.nil?
       return
-    elsif score >= 150
+    elsif score >= 120
       return 'score-elite'
     elsif score >= 100
       return 'score-high'
-    elsif score >= 75
+    elsif score >= 80
       return 'score-good'
-    elsif score >= 50
+    elsif score >= 60
       return 'score-average'
-    elsif score >= 25
+    elsif score >= 40
       return 'score-low'
     else
       return 'score-poor'
