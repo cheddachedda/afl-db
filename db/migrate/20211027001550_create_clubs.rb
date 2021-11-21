664894ae44b8f9a599a0a0aa676af61555e8eb9e
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-21 13:53:56 +1100

    Add :moniker to Club table

--- db/migrate/20211027001550_create_clubs.rb
+++ db/migrate/20211027001550_create_clubs.rb
@@ -2,6 +2,7 @@ class CreateClubs < ActiveRecord::Migration[6.1]
   def change
     create_table :clubs do |t|
       t.string :name
+      t.string :moniker
       t.string :abbreviation
     end
   end
--- db/seeds/01_clubs.rb
+++ db/seeds/01_clubs.rb
@@ -1,28 +1,32 @@
 clubs = [
-  { :name => 'Adelaide Crows', :abbreviation => 'ADE' },
-  { :name => 'Brisbane Lions', :abbreviation => 'BRL' },
-  { :name => 'Carlton Blues', :abbreviation => 'CAR' },
-  { :name => 'Collingwood Magpies', :abbreviation => 'COL' },
-  { :name => 'Essendon Bombers', :abbreviation => 'ESS' },
-  { :name => 'Fremantle Dockers', :abbreviation => 'FRE' },
-  { :name => 'Geelong Cats', :abbreviation => 'GEE' },
-  { :name => 'Gold Coast Suns', :abbreviation => 'GCS' },
-  { :name => 'Greater Western Sydney Giants', :abbreviation => 'GWS' },
-  { :name => 'Hawthorn Hawks', :abbreviation => 'HAW' },
-  { :name => 'Melbourne Demons', :abbreviation => 'MEL' },
-  { :name => 'North Melbourne Kangaroos', :abbreviation => 'NME' },
-  { :name => 'Port Adelaide Power', :abbreviation => 'PTA' },
-  { :name => 'Richmond Tigers', :abbreviation => 'RIC' },
-  { :name => 'St Kilda Saints', :abbreviation => 'STK' },
-  { :name => 'Sydney Swans', :abbreviation => 'SYD' },
-  { :name => 'West Coast Eagles', :abbreviation => 'WCE' },
-  { :name => 'Western Bulldogs', :abbreviation => 'WBD' }
+  { :name => 'Adelaide', :moniker => 'Crows', :abbreviation => 'ADE' },
+  { :name => 'Brisbane', :moniker => 'Lions', :abbreviation => 'BRL' },
+  { :name => 'Carlton', :moniker => 'Blues', :abbreviation => 'CAR' },
+  { :name => 'Collingwood', :moniker => 'Magpies', :abbreviation => 'COL' },
+  { :name => 'Essendon', :moniker => 'Bombers', :abbreviation => 'ESS' },
+  { :name => 'Fremantle', :moniker => 'Dockers', :abbreviation => 'FRE' },
+  { :name => 'Geelong', :moniker => 'Cats', :abbreviation => 'GEE' },
+  { :name => 'Gold Coast', :moniker => 'Suns', :abbreviation => 'GCS' },
+  { :name => 'Greater Western Sydney', :moniker => 'Giants', :abbreviation => 'GWS' },
+  { :name => 'Hawthorn', :moniker => 'Hawks', :abbreviation => 'HAW' },
+  { :name => 'Melbourne', :moniker => 'Demons', :abbreviation => 'MEL' },
+  { :name => 'North Melbourne', :moniker => 'Kangaroos', :abbreviation => 'NME' },
+  { :name => 'Port Adelaide', :moniker => 'Power', :abbreviation => 'PTA' },
+  { :name => 'Richmond', :moniker => 'Tiger', :abbreviation => 'RIC' },
+  { :name => 'St Kilda', :moniker => 'Saints', :abbreviation => 'STK' },
+  { :name => 'Sydney', :moniker => 'Swans', :abbreviation => 'SYD' },
+  { :name => 'West Coast', :moniker => 'Eagles', :abbreviation => 'WCE' },
+  { :name => 'Western Bulldogs', :moniker => 'Bulldogs', :abbreviation => 'WBD' }
 ]
 
 puts "Creating clubs"
 clubs.each do |club|
-  Club.create :name => club[:name], :abbreviation => club[:abbreviation]
-  puts "Created #{ club[:name] }"
+  Club.create(
+    :name => club[:name],
+    :moniker => club[:moniker],
+    :abbreviation => club[:abbreviation]
+  )
+  puts "Created Club: #{ club[:name] }"
 end
 
 puts "#{ Club.count } clubs created"
